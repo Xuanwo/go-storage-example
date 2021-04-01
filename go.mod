2058e762a0e30bd9eb6f7b086e27bb93bb5a88ea
Xuanwo <github@xuanwo.io>
2021-04-01 14:00:07 +0800

    Add example for object storage
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/aos-dev/go-service-fs/v2 v2.0.1-0.20210304102732-f57fecf4e68a
+	github.com/aos-dev/go-service-qingstor/v2 v2.0.1-0.20210316090337-fe1150da51a2
 	github.com/aos-dev/go-storage/v3 v3.4.2
 )
--- main.go
+++ main.go
@@ -4,22 +4,43 @@ import (
 	"bytes"
 	"log"
 
-	"github.com/aos-dev/go-storage/v3/pairs"
 	"github.com/aos-dev/go-service-fs/v2"
+	qingstor "github.com/aos-dev/go-service-qingstor/v2"
+	"github.com/aos-dev/go-storage/v3/pairs"
+	"github.com/aos-dev/go-storage/v3/pkg/credential"
+	"github.com/aos-dev/go-storage/v3/types"
 )
 
-func main() {
+func initFS() (store types.Storager) {
 	// Init a service.
 	store, err := fs.NewStorager(pairs.WithWorkDir("/tmp"))
 	if err != nil {
 		log.Fatalf("service init failed: %v", err)
 	}
 
+	return store
+}
+
+func initQingStor() (store types.Storager) {
+	store, err := qingstor.NewStorager(
+		pairs.WithWorkDir("/tmp"),
+		pairs.WithCredential(credential.NewHmac("access_key", "secret_key").String()),
+	)
+	if err != nil {
+		log.Fatalf("service init failed: %v", err)
+	}
+
+	return store
+}
+
+func main() {
+	store := initFS()
+	// store := initQingStor()
 	content := []byte("Hello, world!")
 	length := int64(len(content))
 	r := bytes.NewReader(content)
 
-	_, err = store.Write("hello", r, length)
+	_, err := store.Write("hello", r, length)
 	if err != nil {
 		log.Fatalf("write failed: %v", err)
 	}
